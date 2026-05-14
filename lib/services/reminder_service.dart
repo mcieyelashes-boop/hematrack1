79ca6b5d8e4ab132fef25bdef78bde83afe08068
mcieyelashes-boop <mci.eyelashes@gmail.com>
2026-05-14 11:45:51 +0700

    Add reminder service stub

---
+++ lib/services/reminder_service.dart
@@ -0,0 +1,13 @@
+class ReminderService {
+  Future<void> initialize() async {
+    // TODO: flutter_local_notifications setup
+  }
+
+  Future<void> scheduleWeeklyReminder() async {
+    // TODO: implement 7-day reminder scheduling
+  }
+
+  Future<void> cancelAllReminders() async {
+    // TODO: cancel notifications
+  }
+}
