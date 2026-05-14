a0dd272b4288dc40a8ecf1a01e334cec45edbc99
mcieyelashes-boop <mci.eyelashes@gmail.com>
2026-05-14 11:35:39 +0700

    Add FollowUpPhoto model

---
+++ lib/models/follow_up_photo.dart
@@ -0,0 +1,15 @@
+class FollowUpPhoto {
+  final String id;
+  final String areaId;
+  final String photoPath;
+  final DateTime date;
+  final double areaRelativeValue;
+
+  FollowUpPhoto({
+    required this.id,
+    required this.areaId,
+    required this.photoPath,
+    required this.date,
+    required this.areaRelativeValue,
+  });
+}
