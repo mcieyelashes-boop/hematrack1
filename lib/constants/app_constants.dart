0c683c0d94cb51d0513734047f2dc937cb5da791
mcieyelashes-boop <mci.eyelashes@gmail.com>
2026-05-14 11:47:37 +0700

    Add app constants

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,8 @@
+class AppConstants {
+  static const String appName = 'HemaTrack';
+  static const String medicalDisclaimer =
+      'Aplikasi ini hanya membantu dokumentasi visual dan bukan alat diagnosis medis.';
+
+  static const double growthThreshold = 0.10;
+  static const int weeklyReminderDays = 7;
+}
