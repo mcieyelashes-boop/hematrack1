c4a27b45862c0414bd69a06896e9e3d5ce754152
mcieyelashes-boop <mci.eyelashes@gmail.com>
2026-05-14 11:52:32 +0700

    Add JSON serialization to HemangiomaArea model

--- lib/models/hemangioma_area.dart
+++ lib/models/hemangioma_area.dart
@@ -4,6 +4,8 @@ class HemangiomaArea {
   final String bodyLocation;
   final String baselinePhotoPath;
   final DateTime baselineDate;
+  final double baselineAreaValue;
+  final String notes;
 
   HemangiomaArea({
     required this.id,
@@ -11,5 +13,29 @@ class HemangiomaArea {
     required this.bodyLocation,
     required this.baselinePhotoPath,
     required this.baselineDate,
+    this.baselineAreaValue = 0,
+    this.notes = '',
   });
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'childId': childId,
+        'bodyLocation': bodyLocation,
+        'baselinePhotoPath': baselinePhotoPath,
+        'baselineDate': baselineDate.toIso8601String(),
+        'baselineAreaValue': baselineAreaValue,
+        'notes': notes,
+      };
+
+  factory HemangiomaArea.fromJson(Map<String, dynamic> json) {
+    return HemangiomaArea(
+      id: json['id'],
+      childId: json['childId'],
+      bodyLocation: json['bodyLocation'],
+      baselinePhotoPath: json['baselinePhotoPath'],
+      baselineDate: DateTime.parse(json['baselineDate']),
+      baselineAreaValue: (json['baselineAreaValue'] as num?)?.toDouble() ?? 0,
+      notes: json['notes'] ?? '',
+    );
+  }
 }
