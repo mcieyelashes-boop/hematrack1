622daed76b9d5602ad15282ab227963cd6ec3b06
mcieyelashes-boop <mci.eyelashes@gmail.com>
2026-05-14 11:35:13 +0700

    Add Child model

---
+++ lib/models/child.dart
@@ -0,0 +1,13 @@
+class Child {
+  final String id;
+  final String name;
+  final DateTime birthDate;
+  final DateTime createdAt;
+
+  Child({
+    required this.id,
+    required this.name,
+    required this.birthDate,
+    required this.createdAt,
+  });
+}
