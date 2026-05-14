82c4c689fe4d4148de5884089f2783f68eaf1ffa
mcieyelashes-boop <mci.eyelashes@gmail.com>
2026-05-14 11:35:24 +0700

    Add HemangiomaArea model

---
+++ lib/models/hemangioma_area.dart
@@ -0,0 +1,15 @@
+class HemangiomaArea {
+  final String id;
+  final String childId;
+  final String bodyLocation;
+  final String baselinePhotoPath;
+  final DateTime baselineDate;
+
+  HemangiomaArea({
+    required this.id,
+    required this.childId,
+    required this.bodyLocation,
+    required this.baselinePhotoPath,
+    required this.baselineDate,
+  });
+}
