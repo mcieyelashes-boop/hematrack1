1e484b11f1744fd860fbf6f80b6b73365fc6c926
mcieyelashes-boop <mci.eyelashes@gmail.com>
2026-05-14 11:46:15 +0700

    Add JSON serialization to Child model

--- lib/models/child.dart
+++ lib/models/child.dart
@@ -10,4 +10,20 @@ class Child {
     required this.birthDate,
     required this.createdAt,
   });
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'birthDate': birthDate.toIso8601String(),
+        'createdAt': createdAt.toIso8601String(),
+      };
+
+  factory Child.fromJson(Map<String, dynamic> json) {
+    return Child(
+      id: json['id'],
+      name: json['name'],
+      birthDate: DateTime.parse(json['birthDate']),
+      createdAt: DateTime.parse(json['createdAt']),
+    );
+  }
 }
